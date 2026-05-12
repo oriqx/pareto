1be527024cbece8571a5e73bdb402a924b88bd5e
eva435 <eva@oriqx.com>
2026-05-12 15:25:39 +0200

    Add AIMD python code

---
+++ tracks/AIMD/constants.py
@@ -0,0 +1,11 @@
+"""Physical constants and element data."""
+
+BOHR_TO_ANG   = 0.529177
+ANG_TO_BOHR   = 1.0 / BOHR_TO_ANG
+AUT_TO_FS     = 0.024188
+FS_TO_AUT     = 1.0 / AUT_TO_FS
+AMU_TO_AU     = 1822.888
+HARTREE_TO_EV = 27.2114
+KB_AU         = 3.166811e-6  # Boltzmann constant in Hartree/K
+
+CHARGES = {"H": 1, "C": 6, "N": 7, "O": 8}
